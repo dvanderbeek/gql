b16758fc65bf0876b691621e1375060f74c1e1c4
David Van Der Beek <earlynovrock@gmail.com>
2024-05-03 12:46:41 -0700

    remove id and timestamps from input type

--- app/graphql/types/inputs/post_input_type.rb
+++ app/graphql/types/inputs/post_input_type.rb
@@ -2,10 +2,7 @@
 
 module Types
   class Inputs::PostInputType < Types::BaseInputObject
-    argument :id, ID, required: false
     argument :title, String, required: false
     argument :content, String, required: false
-    argument :created_at, GraphQL::Types::ISO8601DateTime, required: false
-    argument :updated_at, GraphQL::Types::ISO8601DateTime, required: false
   end
 end
