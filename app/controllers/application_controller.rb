dd3f8cf96718a7fd68bc7da5553d539b242ff8d0
David Van Der Beek <earlynovrock@gmail.com>
2024-05-06 18:49:42 -0700

    abstract resourceful controller, comments on how to fully automate this and we could do something similar for the GQL layer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,70 @@
 class ApplicationController < ActionController::Base
+  before_action :load_resource, only: %i[ show edit update destroy ]
+
+  def index
+    @resources = service.send("load_#{resource_name.to_s.pluralize}")
+  end
+
+  def show
+  end
+
+  def new
+    @resource = resource_klass.new
+  end
+
+  def create
+    success = lambda do |resource|
+      respond_to do |format|
+        format.html { redirect_to @resource, notice: "#{resource_name.to_s.titleize} was successfully created." }
+        format.json { render :show, status: :created, location: @resource }
+      end
+    end
+
+    failure = lambda do |resource|
+      respond_to do |format|
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @resource.errors, status: :unprocessable_entity }
+      end
+    end
+
+    service.send("create_#{resource_name}", permitted_params, success, failure) do |resource|
+      @resource = resource
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    success = lambda do |resource|
+      respond_to do |format|
+        format.html { redirect_to post_url(@resource), notice: "#{resource_name.to_s.titleize}  was successfully updated." }
+        format.json { render :show, status: :ok, location: @resource }
+      end
+    end
+
+    failure = lambda do |resource|
+      respond_to do |format|
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @resource.errors, status: :unprocessable_entity }
+      end
+    end
+
+    service.send("update_#{resource_name}", @resource, permitted_params, success, failure)
+  end
+
+  def destroy
+    @resource.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to resource_klass, notice: "#{resource_name.to_s.titleize} was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def load_resource
+    @resource = service.send("load_#{resource_name}", params[:id])
+  end
 end
--- app/services/blog.rb
+++ app/services/blog.rb
@@ -4,4 +4,16 @@ class Blog
     yield post
     post.valid? ? success.call(post) : failure.call(post)
   end
+
+  def self.update_post(post, attrs, success, failure)
+    post.update(attrs) ? success.call(post) : failure.call(post)
+  end
+
+  def self.load_posts
+    Post.all
+  end
+
+  def self.load_post(id)
+    post = Post.find(id)
+  end
 end
